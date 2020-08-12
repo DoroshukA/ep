110cb659490d084cefa5454c54b77338efe04e9c
Andrey <65175184+DoroshukA@users.noreply.github.com>
2020-08-12 18:17:56 +0300

    HTML

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -26,6 +26,40 @@ export class CarComponent implements OnInit {
     };
     this.options = ["ABS", "autopilot", "Parking"];
   }
+  carSelect(carName) {
+    if(carName == 'bmw') {
+      this.name = 'BMW';
+      this.speed = 688;
+      this.model = 'M5';
+      this.colors = {
+        car: 'yellow',
+        salon: 'btown',
+        wheels: 'black'
+      };
+      this.options = ["масаж", "автопилот", "парктроник",];
+    } else if(carName == 'mer') {
+      this.name = 'Mercedes';
+      this.speed = 323;
+      this.model = 'C180';
+      this.colors = {
+        car: 'green',
+        salon: 'white',
+        wheels: 'red'
+      };
+      this.options = ["АБС", "просмотр фильмов", "Parking"];
+    } else {
+      this.name = 'Audi';
+      this.speed = 552;
+      this.model = 'RS8';
+      this.colors = {
+        car: 'red',
+        salon: 'black',
+        wheels: 'silver'
+    };
+    this.options = ["видеорегистратор", "автопилот", "просмотр фильмов", "масаж"];
+    }
+    
+  }
 }
 
 interface Colors {
